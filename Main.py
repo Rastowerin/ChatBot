ac245bb08cc890f7f0717cc486acd7efea9e1c0e
Rost <rastowerin@gmail.com>
2018-12-16 21:26:46 +0300

    update 1

--- Main.py
+++ Main.py
@@ -10,7 +10,11 @@ while True:
                                                                                             key=key,
                                                                                             ts=ts)).json()
     update = long_poll['updates']
-    if update[0][0] == 4 and  update[0][3] != 321056236 and update[0][6] == 'привет' or 'Привет':
+    if update[0][0] == 4 and update[0][6] == 'привет' or 'Привет':
+        print(update[0][6])
         user_id = update[0][3]
         user_name = vk_bot.method('users.get', {'user_ids': user_id})
         write_msg(user_id, 'привет, ' + (user_name[0]['first_name']))
+    elif update[0][0] == 4 and  update[0][3] == 321056236 and '!дз' in update[0][6]:
+        school_subject = update[0][6] - '!дз'
+        print(school_subject)
