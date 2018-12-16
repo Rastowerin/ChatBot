86d327de82240c2d1e4685660229bb98295e4e6d
Rost <rastowerin@gmail.com>
2018-12-16 20:27:24 +0300

    rollback

--- Config_.py
+++
@@ -1,6 +0,0 @@
-TOKEN = '1beb5d47f77df0d3a46653cbe5a37cb58a846f1a2f8bedebc92c160317d08a5cf2c01b0bb90of06205054a'
-MY_ID = '321056236'
-APP_ID = ''
-API_URL = 'https://api.vk.com/method/'
-ACCESS_TOKEN = ''
-ACCOUNT_TOKEN = ''
