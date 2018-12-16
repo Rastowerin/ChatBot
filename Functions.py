21525a812aeb22a781a1eb91f0d114a4ff738f71
Rost <rastowerin@gmail.com>
2018-12-16 20:26:51 +0300

    update 5

--- Functions.py
+++ Functions.py
@@ -5,22 +5,9 @@ from Config import *
 vk_bot = vk_api.VkApi(token=ACCESS_TOKEN)
 long_poll = vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
 server, key, ts = long_poll['server'], long_poll['key'], long_poll['ts']
-memes_id = vk_bot.method('photos.get', {'owner_id': -66138380, 'album_id': '255161524'})
-meme_id = 'https://vk.com/photo' + memes_id['items']['owner_id'] + '_' + memes_id['items'][random.randint(0, memes_id['count'])]
-vk_bot_user = vk_api.VkApi(token=ACCESS_TOKEN)
 print('готов к работе')
-print(meme_id)
+
 
 
 def write_msg(user_id, text):
     vk_bot.method('messages.send', {'user_id': user_id, 'message': text, 'random_id': random.randint(0, 1000)})
-
-
-def write_msg_attach(user_id, text, att_url):
-    vk_bot.method('messages.send',
-                  {'user_id': user_id, 'attachment': att_url, 'message': text, 'random_id': random.randint(0, 1000)})
-
-
-def get_last_post(owner_id, count, offset, filter):
-    response = vk_bot_user.method('wall.get', {'owner_id': owner_id, 'count': count, 'offset': offset, 'filter': filter})
-    return response['items'][0]['id']
--- Main.py
+++ Main.py
@@ -14,10 +14,3 @@ while True:
         user_id = update[0][3]
         user_name = vk_bot.method('users.get', {'user_ids': user_id})
         write_msg(user_id, 'привет, ' + (user_name[0]['first_name']))
-    elif update[0][0] == 4 and  update[0][3] != 321056236 and 'картинк' in update[0][6]:
-        write_msg_attach(user_id, None, random.choice(memes_id))
-    elif update[0][0] == 4 and  update[0][3] != 321056236 and 'красив' in update[0][6]:
-        group_id = -35684707
-        post_id = get_last_post(group_id, 1, 1, 'owner')
-        attach = 'wall' + str(group_id) + '_' + str(post_id)
-        write_msg_attach(user_id, None, attach)
