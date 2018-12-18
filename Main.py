06df3d81a6969fca11a44a5f50957ddc4f98ce21
rastowerin <Rastowerin@gmail.com>
2018-12-18 17:08:40 +0300

    update 6

--- Main.py
+++ Main.py
@@ -9,12 +9,18 @@ while True:
                                                                                             act='a_check',
                                                                                             key=key,
                                                                                             ts=ts)).json()
+   
     update = long_poll['updates']
-    if update[0][0] == 4 and update[0][6] == 'привет' or 'Привет':
-        print(update[0][6])
-        user_id = update[0][3]
-        user_name = vk_bot.method('users.get', {'user_ids': user_id})
-        write_msg(user_id, 'привет, ' + (user_name[0]['first_name']))
-    elif update[0][0] == 4 and  update[0][3] == 321056236 and '!дз' in update[0][6]:
-        school_subject = update[0][6] - '!дз'
+    #if update[0][0] == 4 and update[0][6] == 'привет' or 'Привет':
+    #    print('1')
+    #    user_id = update[0][3]
+    #    user_name = vk_bot.method('users.get', {'user_ids': user_id})
+    #    write_msg(user_id, 'привет, ' + (user_name[0]['first_name']))
+    if update[0][0] == 4 and  update[0][3] == 321056236 and '!дз' in update[0][6] and update[0][6] != '!дз':
+        school_subject = update[0][6].split(':')[1]
         print(school_subject)
+        str(school_subject)
+        if str(school_subject) in homework:
+            print(1)
+            homework[school_subject] = update[0][6].split(':')
+            print(homework[school_subject])
