f9c62314f90a9d96016b1fd37a70e1be080a092a
DodgeDoge <0_snake_0@mail.ru>
2018-12-26 17:14:25 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 import requests
+import datetime
+d = datetime.date.today()
+tomm = datetime.datetime.isoweekday(d) + 1
 
 from Functions import *
 
@@ -14,3 +17,49 @@ while True:
         user_id = update[0][3]
         user_name = vk_bot.method('users.get', {'user_ids': user_id})
         write_msg(user_id, 'привет, ' + (user_name[0]['first_name']))
+        if 'расписание' in update[0][6]:
+            if tomm == 8:
+                write_msg(user_id, 'Технология, '
+                                   'география, '
+                                   'алгебра, '
+                                   'физика, '
+                                   'химия, '
+                                   'физра')
+            elif tomm == 2:
+                write_msg(user_id, 'Литра, '
+                                   'черчение, '
+                                   'общество, '
+                                   'инглиш, '
+                                   'биология, '
+                                   'геометрия, '
+                                   'кл. час')
+            elif tomm == 3:
+                write_msg(user_id, 'Русский, '
+                                   'инглиш, '
+                                   'физра, '
+                                   'история, '
+                                   'алгебра, '
+                                   'химия')
+            elif tomm == 4:
+                write_msg(user_id, 'Физика, '
+                                   'география, '
+                                   'русский, '
+                                   'инглиш, '
+                                   'обж, '
+                                   'геометрия')
+            elif tomm == 5:
+                write_msg(user_id, 'Музыка, '
+                                   'изо, '
+                                   'инфа, '
+                                   'биология, '
+                                   'русский, '
+                                   'история')
+            elif tomm == 6:
+                write_msg(user_id, 'Литра, '
+                                   'геометрия, '
+                                   'физра, '
+                                   'история, '
+                                   'спб, '
+                                   'алгебра')
+            elif tomm == 7:
+                write_msg(user_id, 'завтра мы не учимся')
